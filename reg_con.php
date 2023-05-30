ae7dd14e4e19cbd6e89ceea85b5fae33edcdc27f
AntPog <anpogorzelczyk@gmail.com>
2023-05-30 14:35:34 +0200

    update connect

--- reg_con.php
+++ reg_con.php
@@ -1,15 +1,6 @@
-<?php
-            require_once "./connect.php";
-            $sql = "SELECT * FROM cities";
-            $result = $conn -> query($sql);
-            while($city = $result -> fetch_assoc()){
-              echo "<option>$city[city]</option>";
-            }
-          ?>
+
 <?php
 
 require_once "./connect.php";
 
-$stmt = $conn->prepare(query: "INSERT INTO `students` (`FirstName`, `LastName`, `Gender`, `Address`, `Password`, `Email`) VALUES (?, ?, ?, ?, ?, ?, ?);");
-
-if($_POST["email"]);
+$stmt = $conn->prepare(query: "INSERT INTO `students` (`FirstName`, `LastName`, `Gender`, `Address`, `Password`, `Email`) VALUES (?, ?, ?, ?, ?, ?);");
