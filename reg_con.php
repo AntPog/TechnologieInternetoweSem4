2407d16097ae3e334ad1ac772250757d523e475f
AntPog <anpogorzelczyk@gmail.com>
2023-05-30 14:48:48 +0200

    update connect

--- reg_con.php
+++ reg_con.php
@@ -4,3 +4,5 @@
 require_once "./connect.php";
 
 $stmt = $conn->prepare(query: "INSERT INTO `students` (`FirstName`, `LastName`, `Gender`, `Address`, `Password`, `Email`) VALUES (?, ?, ?, ?, ?, ?);");
+
+if($_POST["lastName"]);
--- user_registration.php
+++ user_registration.php
@@ -1,10 +1,10 @@
 <?php
-if (isset($_POST['register'])) {
-    $username = $_POST['username'];
+if (isset($_POST['faggot3'])) {
+    $LastName = $_POST['Email'];
     $password = $_POST['password'];
 
     // Sprawdzanie, czy użytkownik o podanej nazwie już istnieje
-    $checkQuery = "SELECT * FROM users WHERE username='$username'";
+    $checkQuery = "SELECT * FROM students WHERE Email='$LastName'";
     $result = $conn->query($checkQuery);
 
     if ($result->num_rows > 0) {
@@ -12,7 +12,7 @@ if (isset($_POST['register'])) {
     } else {
         // Dodawanie użytkownika do bazy danych
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        $insertQuery = "INSERT INTO users (username, password) VALUES ('$username', '$hashedPassword')";
+        $insertQuery = "INSERT INTO students (Email, password) VALUES ('$LastName', '$hashedPassword')";
 
         if ($conn->query($insertQuery) === TRUE) {
             echo "Rejestracja zakończona sukcesem.";
