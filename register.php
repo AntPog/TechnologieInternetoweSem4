de45656d76cc1deec66bc98527499b91173a8cb4
NatalieJoan <nhelak2001@gmail.com>
2023-05-30 13:10:39 +0200

    Update register.php

--- register.php
+++ register.php
@@ -6,11 +6,92 @@
 <body>
     <h2>Rejestracja</h2>
     <form action="register.php" method="post">
-        <label for="username">Nazwa użytkownika:</label>
-        <input type="text" id="username" name="username" required><br><br>
-        <label for="password">Hasło:</label>
-        <input type="password" id="password" name="password" required><br><br>
-        <input type="submit" name="register" value="Zarejestruj">
-    </form>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Imię" name="firstName">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Nazwisko" name="lastName">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Email" name="email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Potwierdź imię" name="confirmEmail">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Hasło" name="password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Potwierdź hasło" name="confirmPassword">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="date" class="form-control" name="birthday">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-calendar-alt"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <select class="form-control" name="city_id">
+            <option>Poznań</option>
+            <option>Szczecin</option>
+            <option>Warszawa</option>
+            <option>Łódź</option>
+            <option>Wrocław</option>
+            <option>Katowice</option>
+          </select>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-city"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+              <label for="agreeTerms">
+              I agree to the <a href="#">terms</a>
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Register</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
 </body>
 </html>
